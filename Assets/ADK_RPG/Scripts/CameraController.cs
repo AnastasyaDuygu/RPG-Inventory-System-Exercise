af0bcf686e5340a6b9a3042259f4fe2d6bcae975
AnastasyaDuygu <anastasyaduygu@gmail.com>
2024-02-29 14:36:31 +0300

    (camera) add : rotation with a & d keys

--- Assets/ADK_RPG/Scripts/CameraController.cs
+++ Assets/ADK_RPG/Scripts/CameraController.cs
@@ -4,15 +4,28 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform target;
     public Vector3 offset; //camera offset
-    private float currentZoom = 10f;
+
+    
     private float zoomSpeed = 4f;
+    private float minZoom = 5f;
+    private float currentZoom = 10f;
+    private float maxZoom = 15f;
+
+    private float currentRotation = 0f;
+    private float rotationSpeed = 50f;
+
     private void Update()
     {
         currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
-        currentZoom = Mathf.Clamp(currentZoom, 5f, 15f);
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        currentRotation += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
     }
     private void LateUpdate()
     {
         transform.position = target.position - offset * currentZoom;
+
+        transform.LookAt(target.position + Vector3.up * 2); //this is needed for rotation
+        transform.RotateAround(target.position, Vector3.up, currentRotation);
     }
 }
