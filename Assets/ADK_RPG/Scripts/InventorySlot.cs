29461ba6882995cdbd2e65e89c84fa57efca7e01
AnastasyaDuygu <anastasyaduygu@gmail.com>
2024-03-01 16:54:12 +0300

    (inventory slot) update : when slot is clicked we remove it from the inventory

--- Assets/ADK_RPG/Scripts/InventorySlot.cs
+++ Assets/ADK_RPG/Scripts/InventorySlot.cs
@@ -4,18 +4,28 @@ public class InventorySlot : MonoBehaviour
 {
     [SerializeField] Image icon;
     Item item;
+
+    bool isEmpty = true;
+
     public void AddItem(Item newItem)
     {
         item = newItem;
         icon.sprite = item.icon;
         icon.enabled = true;
+        isEmpty = false;
     }
-
     public void ClearSlot()
     {
         item = null;
 
         icon.sprite = null;
         icon.enabled = false;
+        isEmpty = true;
+    }
+    public void OnSlotButtonClicked()
+    {
+        if (isEmpty) return; // do nothing if slot is empty
+        //
+        Inventory.Instance.Remove(item);
     }
 }
