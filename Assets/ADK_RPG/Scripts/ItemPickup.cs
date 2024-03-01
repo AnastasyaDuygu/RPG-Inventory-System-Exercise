434f417bddcad4f4dc3f5dbcd4a277242f09ae87
AnastasyaDuygu <anastasyaduygu@gmail.com>
2024-03-01 11:52:07 +0300

    (inventory) add : inventory script with singleton instance  and delegate event

--- Assets/ADK_RPG/Scripts/ItemPickup.cs
+++ Assets/ADK_RPG/Scripts/ItemPickup.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class ItemPickup : Interactable
 {
+    [SerializeField] private Item item;
     public override void Interact()
     {
         base.Interact();
-
         PickUp();
     }
 
     private void PickUp()
     {
-        Debug.Log("Picking up item");
-        Destroy(gameObject);
+        Debug.Log("Picking up " + item.name);
+        bool wasPickedUp = Inventory.Instance.Add(item);
+        if (wasPickedUp)
+            Destroy(gameObject);
+
     }
 }
