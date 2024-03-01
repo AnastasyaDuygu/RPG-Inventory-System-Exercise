c7b0fbd805df2438ee8a27d279cd0391aa2aba75
AnastasyaDuygu <anastasyaduygu@gmail.com>
2024-03-01 16:38:42 +0300

    (inventory slot) update :  ui updates as we pick up items

---
+++ Assets/ADK_RPG/Scripts/AlwaysOpenInventory.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class AlwaysOpenInventory : MonoBehaviour
+{
+    //when ui is update and if all 7 slots of always open inventory is not full project item to always open inventory
+}
--- Assets/ADK_RPG/Scripts/InventoryUI.cs
+++ Assets/ADK_RPG/Scripts/InventoryUI.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
+    [SerializeField] Transform slotsParent;
+    InventorySlot[] slots;
+
     Inventory inventory;
 
     void Start()
     {
+        slots = slotsParent.GetComponentsInChildren<InventorySlot>();
         inventory = Inventory.Instance;
         inventory.onItemChangedCallback += UpdateUI; //subscribing method to the event, now UpdateUI will be called when event is triggered
     }
@@ -17,5 +21,16 @@ public class InventoryUI : MonoBehaviour
     void UpdateUI()
     {
         Debug.Log("Updating UI");
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.items.Count) //there are items to add
+            {
+                slots[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();    
+            }
+        }
     }
 }
