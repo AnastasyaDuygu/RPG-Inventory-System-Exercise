951c20e33e4c20407f2a6c4663faeef3cf32776c
AnastasyaDuygu <anastasyaduygu@gmail.com>
2024-03-04 11:41:08 +0300

    (inventory) fix : fixed inventory system again...

--- Assets/ADK_RPG/Scripts/InventoryUI.cs
+++ Assets/ADK_RPG/Scripts/InventoryUI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
@@ -6,16 +7,18 @@ public class InventoryUI : MonoBehaviour
     [SerializeField] Transform slotsAlwaysOpenParent;
     [SerializeField] Transform slotsParent;
     [SerializeField] GameObject inventoryCanvas;
-    public InventorySlot[] slotsAlwaysOpen;
+    InventorySlot[] slotsAlwaysOpen;
     InventorySlot[] slots;
+    public InventorySlot[] allSlots;
 
     Inventory inventory;
 
     void Start()
     {
-        slots = slotsParent.GetComponentsInChildren<InventorySlot>();
-        //
         slotsAlwaysOpen = slotsAlwaysOpenParent.GetComponentsInChildren<InventorySlot>();
+        slots = slotsParent.GetComponentsInChildren<InventorySlot>();
+        allSlots = slotsAlwaysOpen.Concat(slots).ToArray();
+        //
         inventory = Inventory.Instance;
         inventory.onItemChangedCallback += UpdateUI; //subscribing method to the event, now UpdateUI will be called when event is triggered
     }
@@ -29,44 +32,19 @@ public class InventoryUI : MonoBehaviour
     void UpdateUI()
     {
         Debug.Log("Updating UI");
-        var lastSlot = slotsAlwaysOpen[slotsAlwaysOpen.Length - 1];
-        if (lastSlot.isEmpty) //if always open slots are NOT full
-        {
-            Debug.Log("ALWAYS OPEN NOT FULL");
-            for (int i = 0; i < slotsAlwaysOpen.Length; i++)
-            {
-                if (i < inventory.items.Count) //there are items to add
-                {
-                    slotsAlwaysOpen[i].AddItem(inventory.items[i]);
-                }
-                else
-                {
-                    slotsAlwaysOpen[i].ClearSlot();
-                }
-            }
-        }
-        else //if always open slots ARE full
-        {
-            Debug.Log("ALWAYS OPEN IS FULL");
-            for (int i = 0;i < slots.Length; i++)
-            {
-                if (i < inventory.items.Count - slotsAlwaysOpen.Length) //there are items to add
-                {
-                    slots[i].AddItem(inventory.items[i]);
-                }
-                else
-                {
-                    slots[i].ClearSlot();
-                }
-            }
-        }
-        
-    }
-    /*public InventorySlot[] getSlots()
-    {
-        return slotsAlwaysOpen;
-    }*/
 
+        for (int i = 0; i < allSlots.Length; i++)
+        {
+            if (i < inventory.items.Count) //there are items to add
+            {
+                allSlots[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                allSlots[i].ClearSlot();
+            }
+        }
+    }
     public void OnCloseButtonClicked()
     {
         inventoryCanvas.SetActive(false);
